2095ae4ba1a6a6a1e9240b6c7aee67f2aa1024cd
Peter Jung <admin@ptr1337.dev>
2023-02-06 17:21:00 +0100

    Change calamares package name to not break old ISOs
    Signed-off-by: Peter Jung <admin@ptr1337.dev>

--- archiso/airootfs/usr/local/bin/calamares-online.sh
+++ archiso/airootfs/usr/local/bin/calamares-online.sh
@@ -59,14 +59,14 @@ Main() {
     if [[ "${_exitcode}" -eq 2 ]]; then
         BOOTLOADER="GRUB"
         echo "USING GRUB!"
-        yes | sudo pacman -R cachyos-calamares-systemd
-        yes | sudo pacman -R cachyos-calamares-grub
-        yes | sudo pacman -Sy cachyos-calamares-grub
+        yes | sudo pacman -R cachyos-calamares-next-systemd
+        yes | sudo pacman -R cachyos-calamares-next-grub
+        yes | sudo pacman -Sy cachyos-calamares-next-grub
     elif [[ "${_exitcode}" -eq 3 ]]; then
         BOOTLOADER="SYSTEMD-BOOT"
         echo "USING SYSTEMD-BOOT!"
-        yes | sudo pacman -R cachyos-calamares-grub
-        yes | sudo pacman -Sy cachyos-calamares-systemd
+        yes | sudo pacman -R cachyos-calamares-next-grub
+        yes | sudo pacman -Sy cachyos-calamares-next-systemd
     else
         exit
     fi
