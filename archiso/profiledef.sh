103a1fd14ef8710a2a628d1b9a59351821340c5d
Peter Jung <admin@ptr1337.dev>
2024-05-03 19:16:39 +0200

    Deckify: Add kde config and screenrotate
    Signed-off-by: Peter Jung <admin@ptr1337.dev>

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -33,4 +33,5 @@ file_permissions=(
   ["/usr/local/bin/removeun"]="0:0:755"
   ["/usr/local/bin/removeun-online"]="0:0:755"
   ["/usr/local/bin/prepare-live-desktop.sh"]="0:0:755"
+  ["/usr/bin/deckscale"]="0:0:755"
 )
