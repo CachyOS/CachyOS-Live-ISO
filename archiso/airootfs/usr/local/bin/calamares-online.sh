60fb4db6a3c554b78050aa63d12d091bc8c26f85
Vasiliy Stelmachenok <ventureo@cachyos.org>
2025-05-20 14:25:15 +0300

    Drop GRUB and use Limine by default
    Signed-off-by: Vasiliy Stelmachenok <ventureo@cachyos.org>

--- archiso/airootfs/usr/local/bin/calamares-online.sh
+++ archiso/airootfs/usr/local/bin/calamares-online.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-Main() {
+main() {
     # Remove current keyring first, to complete initiate it
     sudo rm -rf /etc/pacman.d/gnupg
     # We are using this, because archlinux is signing the keyring often with a newly created keyring
@@ -22,86 +22,56 @@ Main() {
     local mode="online"  # TODO: keep this line for now
 
     local _efi_check_dir="/sys/firmware/efi"
-    local _exitcode=2 # by default use grub
+    local _exitcode=6 # by default use limine
 
-    local SYSTEM=""
-    local BOOTLOADER=""
+    local system="BIOS/MBR SYSTEM"
+    local bootloader=""
+
     if [ -d "${_efi_check_dir}" ]; then
-        SYSTEM="UEFI SYSTEM"
+        system="UEFI SYSTEM"
         _exitcode=$(yad --width 300 --title "Bootloader" \
     --image=gnome-shutdown \
-    --button="Grub:2" \
-    --button="Systemd-boot(Default):3" \
+    --button="Systemd-boot:3" \
     --button="Refind:4" \
     --button="AI SDK / Refind:5" \
-    --button="Limine:6" \
-    --text "Choose Bootloader/Edition:" ; echo $?)
-    else
-        SYSTEM="BIOS/MBR SYSTEM"
-        _exitcode=$(yad --width 300 --title "Bootloader" \
-    --image=gnome-shutdown \
-    --button="Grub:2" \
-    --button="Limine:6" \
+    --button="Limine (Default):6" \
     --text "Choose Bootloader/Edition:" ; echo $?)
     fi
 
-    local ISO_VERSION="$(cat /etc/version-tag)"
-    echo "USING ISO VERSION: ${ISO_VERSION}"
+    local -A bootloaders=(
+        ["3"]="systemd-boot"
+        ["4"]="rEFInd"
+        ["5"]="AI SDK / rEFInd"
+        ["6"]="Limine"
+    )
 
-    if [[ "${_exitcode}" -eq 2 ]]; then
-        BOOTLOADER="GRUB"
-        echo "USING GRUB!"
-        yes | sudo pacman -R cachyos-calamares-qt6-next-systemd
-        yes | sudo pacman -R cachyos-calamares-qt6-next-grub
-        yes | sudo pacman -R cachyos-calamares-qt6-next-refind
-        yes | sudo pacman -R cachyos-calamares-qt6-next-limine
-        yes | sudo pacman -Sy cachyos-calamares-qt6-next-grub
-    elif [[ "${_exitcode}" -eq 3 ]]; then
-        BOOTLOADER="SYSTEMD-BOOT"
-        echo "USING SYSTEMD-BOOT!"
-        yes | sudo pacman -R cachyos-calamares-qt6-next-grub
-        yes | sudo pacman -R cachyos-calamares-qt6-next-refind
-        yes | sudo pacman -R cachyos-calamares-qt6-next-limine
-        yes | sudo pacman -Sy cachyos-calamares-qt6-next-systemd
-    elif [[ "${_exitcode}" -eq 4 ]]; then
-        BOOTLOADER="REFIND"
-        echo "USING REFIND!"
-        yes | sudo pacman -R cachyos-calamares-qt6-next-grub
-        yes | sudo pacman -R cachyos-calamares-qt6-next-systemd
-        yes | sudo pacman -R cachyos-calamares-qt6-next-limine
-        yes | sudo pacman -Sy cachyos-calamares-qt6-next-refind
-    elif [[ "${_exitcode}" -eq 5 ]]; then
-        BOOTLOADER="AI-SDK/Refind"
-        echo "USING AI SDK and Refind!"
-        yes | sudo pacman -R cachyos-calamares-qt6-next-grub
-        yes | sudo pacman -R cachyos-calamares-qt6-next-systemd
-        yes | sudo pacman -R cachyos-calamares-qt6-next-refind
-        yes | sudo pacman -R cachyos-calamares-qt6-next-limine
-        yes | sudo pacman -Sy cachyos-calamares-qt6-next-ai
-    elif [[ "${_exitcode}" -eq 6 ]]; then
-        BOOTLOADER="Limine"
-        echo "USING Limine"
-        yes | sudo pacman -R cachyos-calamares-qt6-next-grub
-        yes | sudo pacman -R cachyos-calamares-qt6-next-systemd
-        yes | sudo pacman -R cachyos-calamares-qt6-next-refind
-        yes | sudo pacman -Sy cachyos-calamares-qt6-next-limine
-    else
-        exit
-    fi
+    local iso_version="$(cat /etc/version-tag)"
+    echo "USING ISO VERSION: ${iso_version}"
+
+    case "${_exitcode}" in
+        3) yes | sudo pacman -Sy cachyos-calamares-qt6-next-systemd;;
+        4) yes | sudo pacman -Sy cachyos-calamares-qt6-next-refind;;
+        5) yes | sudo pacman -Sy cachyos-calamares-qt6-next-ai;;
+        6) yes | sudo pacman -Sy cachyos-calamares-qt6-next-limine;;
+        *) exit;;
+    esac
+
+    bootloader="${bootloaders[$_exitcode]}"
+    echo "Using ${bootloader}!"
 
     # Get Hardware Informations
-    inxi -F > $log
+    inxi -F > "$log"
 
-    cat <<EOF >> $log
+    cat <<EOF >> "$log"
 ########## $log by $progname
 ########## Started (UTC): $(date -u "+%x %X")
-########## ISO version: $ISO_VERSION
-########## System: $SYSTEM
-########## Bootloader: $BOOTLOADER
+########## ISO version: $iso_version
+########## System: $system
+########## Bootloader: $bootloader
 EOF
 
     sudo cp "/usr/share/calamares/settings_${mode}.conf" /etc/calamares/settings.conf
-    sudo -E dbus-launch calamares -D6 >> $log &
+    sudo -E dbus-launch calamares -D6 >> "$log" &
 }
 
-Main "$@"
+main "$@"
