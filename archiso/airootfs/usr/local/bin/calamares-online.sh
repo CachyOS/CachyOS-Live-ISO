92959ef86b3006cb14f128ecc152455d8a59aee2
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2026-07-29 00:43:13 +0400

    packages: update calamares pkgname

--- archiso/airootfs/usr/local/bin/calamares-online.sh
+++ archiso/airootfs/usr/local/bin/calamares-online.sh
@@ -32,7 +32,7 @@ main() {
     local ISO_VERSION="$(cat /etc/version-tag)"
     echo "USING ISO VERSION: ${ISO_VERSION}"
 
-    sudo pacman -Sy --noconfirm cachyos-calamares
+    sudo pacman -Sy --noconfirm cachyos-calamares-next
 
     # Get Hardware Informations
     inxi -F > "$log"
